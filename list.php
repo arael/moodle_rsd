a5ccbda05dc1680adc02599177350a1a8d6609fd
Goran Josic <goran.josic@usi.ch>
2016-03-07 17:55:02 +0100

    Including other services is now possible.
    It is now possible to add new service descriptions.
    The Readme.txt contains all the info needed to add a new service
    description.

--- list.php
+++ list.php
@@ -1,31 +1,57 @@
 <?php
-require_once('../../config.php');
-require_once($CFG->libdir . '/adminlib.php');
-require_once($CFG->libdir . '/externallib.php');
-require_once($CFG->dirroot . '/webservice/lib.php');
-require_once($CFG->dirroot . '/admin/webservice/forms.php');
-// rsd local lib
-require_once('lib.php');
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * @package   local_rsd
+ * @copyright 2016, Goran Josic <goran.josic@usi.ch>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-// get all enabled web services
-$services = $DB->get_records('external_services', array('enabled' => 1));
+//*************************************************************
+// How does this work?
+// Scan all the sub directories in the services directory.
+// If they contains rsd.php include it in the array.
+// Convert the array in JSON and print it as output.
+//*************************************************************
 
-$active_protocols = empty($CFG->webserviceprotocols) ?  array() : explode(',', $CFG->webserviceprotocols);
+// directory containing all the service descriptions.
+$services = 'services';
+// Scan services directory. Ignore the .. and . entries.
+$scanned_services = array_diff(scandir($services), array('..', '.'));
 
-$output = array(
-	'engine' => array(
-		'type' => 'Moodle',
-		'version' => $CFG->release,
-		'name' => 'Moodle',
-		'link' => $CFG->wwwroot
-	),
-	'services' => array()
-);
-foreach($services as $service) {
-	foreach($active_protocols as $protocol) {
-		$output['services'][] = get_service_description($service, $protocol);
+$services_description = array();
+foreach($scanned_services as $service) {
+	// Check if the entry it is a directory.
+	$path = $services.DIRECTORY_SEPARATOR.$service;
+	if(is_dir($path)) {
+		// Check if the entry contains an rsd.php file.
+		// If the file is a rsd.php include it.
+		$rsd = $path.DIRECTORY_SEPARATOR.'rsd.php';
+		if(file_exists($rsd)) {
+			// include the static class
+			include($rsd);
+			// use the convention service_directory_name + _rsd as class name
+			$class = basename($path).'_rsd';
+			// call the describe function and get the standard array illustrated in services/Readme.txt file
+			$services_description[] = $class::describe();
+		}
 	}
 }
+
+// encode in JSON and print out.
 header('Content-type: application/json');
-echo json_encode($output);
+echo json_encode($services_description);
 ?>
---
+++ services/web_services/rsd.php
@@ -0,0 +1,69 @@
+<?php
+require_once('../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+require_once($CFG->libdir . '/externallib.php');
+require_once($CFG->dirroot . '/webservice/lib.php');
+require_once($CFG->dirroot . '/admin/webservice/forms.php');
+// rsd local lib
+/* require_once('lib.php'); */
+
+
+class web_services_rsd {
+	public static function get_service_description($service, $protocol) {
+		global $CFG;
+		/* return $service; */
+		$webservicemanager = new webservice();
+		$functions = $webservicemanager->get_external_functions($service->id);
+		/* return $functions[8]; // inspect the function info field */
+		/* $function_info = external_function_info($functions[8]); */
+		/* return $function_info; // inspect the function info field */
+		$functions_details = array();
+		foreach($functions as $function) {
+			$info = external_function_info($function);
+			$function_details[] = array(
+				'name' => $info->name,
+				/* 'link' => '?wsfunction='.$info->name, */
+				'description' => $info->description
+			);
+		}
+		return array(
+			'service' => array(
+				'servicename' => $service->name,
+				'servicelink' => $CFG->wwwroot . "/webservice/$protocol/server.php",
+				'version' => $CFG->release,
+				'protocol' => $protocol
+			),
+			'apis' => $function_details
+		);
+	}
+
+	public static function describe() {
+		global $DB, $CFG;
+		// get all enabled web services
+		$services = $DB->get_records('external_services', array('enabled' => 1));
+
+		$active_protocols = empty($CFG->webserviceprotocols) ?  array() : explode(',', $CFG->webserviceprotocols);
+
+		$output = array(
+			'engine' => array(
+				'type' => 'Moodle',
+				'version' => $CFG->release,
+				'name' => 'Moodle',
+				'link' => $CFG->wwwroot
+			),
+			'services' => array()
+		);
+		foreach($services as $service) {
+			foreach($active_protocols as $protocol) {
+				$output['services'][] = self::get_service_description($service, $protocol);
+			}
+		}
+		return $output;
+	}
+}
+
+/* header('Content-type: application/json'); */
+/* echo json_encode($output); */
+/* return json_encode($output); */
+/* $service_description[] = $output; */
+?>
